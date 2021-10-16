cf58d6d7b5819ee5efeb91e0466e9228bf6e4b18
Ivan <cicain90@gmail.com>
2021-10-15 21:51:15 -0700

    Finished Ac1 of User Story 1
    Fixed TestRunner and FailedTestRunner glue
    was:
    glue = "com/cydeo/step_definitions"
    now is:
    glue = "com/trycloud/step_definitions"

--- src/test/java/com/trycloud/pages/CommonPOM.java
+++ src/test/java/com/trycloud/pages/CommonPOM.java
@@ -1,6 +1,9 @@
 package com.trycloud.pages;
 
+import com.sun.org.apache.bcel.internal.generic.BREAKPOINT;
+import com.trycloud.utilities.BrowserUtil;
 import com.trycloud.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -19,6 +22,7 @@ public class CommonPOM {
 
     public void logOut(){
         this.logoutDrop.click();
+        BrowserUtil.waitForVisibility(By.xpath("//li[@ data-id='logout']"),3);
         this.logoutBtn.click();
     }
 
