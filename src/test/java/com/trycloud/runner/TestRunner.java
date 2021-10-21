2f429d35dbe896537d1cf6f1cc038131ffd857f9
AbrahamWang <wang.ibrahim222@gmail.com>
2021-10-20 17:14:52 -0700

    Update TestRunner.java
    modify

--- src/test/java/com/trycloud/runner/TestRunner.java
+++ src/test/java/com/trycloud/runner/TestRunner.java
@@ -27,7 +27,7 @@ import org.junit.runner.RunWith;
                 "me.jvt.cucumber.report.PrettyReports:target"  // fancy report
         } ,
 
-       // tags = "",
+        tags = "@wang",
         dryRun = false
 )
 
