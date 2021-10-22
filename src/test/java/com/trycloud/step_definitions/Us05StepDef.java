28dd6706d4ae0d098d29de33b41d35d12466e519
AbrahamWang <wang.ibrahim222@gmail.com>
2021-10-22 00:05:40 -0700

    Update Us05StepDef.java
    updated

--- src/test/java/com/trycloud/step_definitions/Us05StepDef.java
+++ src/test/java/com/trycloud/step_definitions/Us05StepDef.java
@@ -3,9 +3,11 @@ package com.trycloud.step_definitions;
 import com.trycloud.pages.CommonPOM;
 import com.trycloud.pages.FilesPage;
 import com.trycloud.pages.LoginPagePOM;
+import com.trycloud.utilities.BrowserUtil;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
 
 import static org.junit.Assert.*;
 
@@ -21,6 +23,7 @@ public class Us05StepDef {
     @When("Click action-icon from any file on the page")
     public void click_action_icon_from_any_file_on_the_page() {
         commonPOM.clickModule("Files");
+        BrowserUtil.waitForVisibility(By.xpath("//tbody[@id='fileList']/tr"),1);
     }
 
     @When("Click “Remove from Favorites” option")
@@ -35,8 +38,8 @@ public class Us05StepDef {
     }
 
 
-
-
-
-
+    @When("Click the “plus+” icon on top")
+    public void clickThePlusIconOnTop() {
+        commonPOM.clickModule("Files");
+    }
 }
